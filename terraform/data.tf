1363d05cb4b0be321cefb77a254f1af8c5a89a1e
William Fish <william.michael.fish@gmail.com>
2024-07-09 17:17:46 +0100

    FPO-318: Adds initial terraform configuration and circle ci workflows
    - Adds terraform configuration (backend and tfvars have been configured)
    - Adds circle ci configuration (not configured, still needs to be doned)
    - Still need to review data.tf and locals.tf

---
+++ terraform/data.tf
@@ -0,0 +1,58 @@
+data "aws_vpc" "vpc" {
+  tags = { Name = "trade-tariff-${var.environment}-vpc" }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
+
+  tags = {
+    Name = "*private*"
+  }
+}
+
+data "aws_lb_target_group" "this" {
+  name = "hub-backend"
+}
+
+data "aws_security_group" "this" {
+  name = "trade-tariff-ecs-security-group-${var.environment}"
+}
+
+data "aws_kms_key" "secretsmanager_key" {
+  key_id = "alias/secretsmanager-key"
+}
+
+data "aws_ssm_parameter" "ecr_url" {
+  name = "/${var.environment}/FPO_DEVELOPER_HUB_BACKEND_ECR_URL"
+}
+
+data "aws_dynamodb_table" "audit_log" {
+  name = "AuditLog"
+}
+
+data "aws_dynamodb_table" "customer_api_keys" {
+  name = "CustomerApiKeys"
+}
+
+data "aws_dynamodb_table" "organisations" {
+  name = "Organisations"
+}
+
+data "aws_dynamodb_table" "users" {
+  name = "Users"
+}
+
+data "aws_secretsmanager_secret" "encryption_key" {
+  name = "dev-hub-backend-encryption-key"
+}
+
+data "aws_secretsmanager_secret" "sentry_dsn" {
+  name = "dev-hub-backend-sentry-dsn"
+}
+
+data "aws_ssm_parameter" "cognito_public_keys_url" {
+  name = "/${var.environment}/FPO_DEVELOPER_HUB_COGNITO_PUBLIC_USER_URL"
+}
---
+++ terraform/iam.tf
@@ -0,0 +1,95 @@
+data "aws_iam_policy_document" "exec" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:Encrypt",
+      "kms:GenerateDataKeyPair",
+      "kms:GenerateDataKeyPairWithoutPlainText",
+      "kms:GenerateDataKeyWithoutPlaintext",
+      "kms:ReEncryptFrom",
+      "kms:ReEncryptTo",
+    ]
+    resources = [
+      data.aws_kms_key.secretsmanager_key.arn
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetResourcePolicy",
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:ListSecretVersionIds"
+    ]
+    resources = [
+      data.aws_secretsmanager_secret.encryption_key.arn,
+      data.aws_secretsmanager_secret.sentry_dsn.arn
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:DescribeParameters",
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+    ]
+    resources = [
+      data.aws_ssm_parameter.cognito_public_keys_url.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "exec" {
+  name   = "${local.service}-execution-role-policy"
+  policy = data.aws_iam_policy_document.exec.json
+}
+
+data "aws_iam_policy_document" "task" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:DescribeLogStreams",
+      "logs:PutLogEvents",
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:BatchGetItem",
+      "dynamodb:BatchWriteItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:Query",
+      "dynamodb:Scan",
+      "dynamodb:UpdateItem",
+    ]
+    resources = [
+      data.aws_dynamodb_table.audit_log.arn,
+      data.aws_dynamodb_table.customer_api_keys.arn,
+      data.aws_dynamodb_table.organisations.arn,
+      data.aws_dynamodb_table.users.arn
+    ]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["apigateway:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "task" {
+  name   = "${local.service}-task-role-policy"
+  policy = data.aws_iam_policy_document.task.json
+}
