7c8fc86854d23ef369fa6d56dd44fabd7c28f51b
Moussa Kayad <mkayad@gmail.com>
2024-07-30 16:17:18 +0100

    FPO-351 add confirmation page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,7 @@ const healthchecksController = new HealthchecksController()
 
 router.get('/healthcheck', (req, res) => { healthchecksController.show(req, res) })
 router.get('/healthcheckz', (req, res) => { healthchecksController.showz(req, res) })
+router.get('/confirmation', (req, res) => { res.render('confirmation') })
 
 export default router.get('/', (_req: Request, res: Response, _next: NextFunction) => {
   res.render('index')
