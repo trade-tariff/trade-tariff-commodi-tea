3dfbbe4d54426f2b8fc9bc2a046ef93787f4b569
William Fish <william.michael.fish@gmail.com>
2024-09-12 10:58:19 +0100

    FPO-356: Restrict access to update identification owned by user

--- src/controllers/improveController.ts
+++ src/controllers/improveController.ts
@@ -1,6 +1,7 @@
 import { type Request, type Response } from 'express'
 import Identification from '../models/identification'
 import { logger } from '../config/logging'
+import { type CognitoUser, UserService } from '../services/userService'
 
 export class ImproveController {
   public show (req: Request, res: Response): void {
@@ -11,27 +12,36 @@ export class ImproveController {
   }
 
   public async update (req: Request, res: Response): Promise<void> {
+    const user: CognitoUser = UserService.call(req)
     const identificationId = req.params.id
     const reason = req.body.reason
 
     logger.debug(`Updating identification ${identificationId} with reason ${reason}`)
-    await Identification.update(
-      {
-        answer: {
-          answer: 'no',
-          answer_info: {
-            reason
-          }
-        },
-        state: 'completed'
-      },
-      {
-        where: {
-          id: identificationId
-        }
-      }
-    )
 
-    res.redirect('/confirmation')
+    try {
+      await Identification.update(
+        {
+          answer: {
+            answer: 'no',
+            answer_info: {
+              reason
+            }
+          },
+          state: 'completed'
+        },
+        {
+          where: {
+            id: identificationId,
+            userId: user.userId
+          }
+        }
+      )
+      res.redirect('/confirmation')
+    } catch (error) {
+      logger.error(`Failed to update identification ${identificationId} with reason ${reason}`)
+      logger.error(error)
+      res.status(500)
+      res.render('500')
+    }
   }
 }
